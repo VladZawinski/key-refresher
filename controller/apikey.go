b75e225fbe3e913898161a26420317338a729943
Thiha Kaung Sett <thihakaungsett@zig.live>
2022-09-08 15:38:10 +0630

    Added Echo

--- controller/apikey.go
+++ controller/apikey.go
@@ -1,4 +1,23 @@
 package controller
 
+import (
+	"context"
+
+	"github.com/token-refresher/ent"
+	"github.com/token-refresher/ent/apikey"
+)
+
 type ApiKeyController struct {
+	client *ent.Client
+}
+
+func (controller ApiKeyController) GetFreshAPIKey(ctx context.Context) (*ent.ApiKey, error) {
+	current, err := controller.client.ApiKey.
+		Query().Where(apikey.StatusIn(apikey.StatusInUse)).Only(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return current, nil
 }
--- internal/livecoinwatch.go
+++ internal/livecoinwatch.go
@@ -9,18 +9,6 @@ import (
 	"strings"
 )
 
-type ListingResponse []struct {
-	Coin Coin
-}
-type ListingBody struct {
-	Currency string `json:"currency"`
-	Sort     string `json:"sort"`
-	Order    string `json:"order"`
-	Offset   int    `json:"offset"`
-	Limit    int    `json:"limit"`
-	Meta     bool   `json:"meta"`
-}
-
 type Coin struct {
 	Code string  `json:"code"`
 	Rate float32 `json:"rate"`
@@ -38,29 +26,21 @@ func GetListing() []Coin {
 	}`)
 
 	request, err := http.NewRequest(http.MethodPost, "https://api.livecoinwatch.com/coins/list/", payload)
-	request.Header.Add("x-api-key", "300842c5-aee8-4a55-8da9-efc75c6d8bcc")
-	request.Header.Add("content-type", "application/json")
 	if err != nil {
 		fmt.Print(err)
 	}
+	request.Header.Add("x-api-key", "300842c5-aee8-4a55-8da9-efc75c6d8bcc")
+	request.Header.Add("content-type", "application/json")
 
 	resp, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer resp.Body.Close()
 
 	var coins []Coin
 	responseBody, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(responseBody, &coins)
-	defer resp.Body.Close()
 
 	return coins
-}
-
-func FakeParse() {
-	b := []byte(`{"code": "BTC", "rate": 19212.045766750387}`)
-	var coin Coin
-	json.Unmarshal(b, &coin)
-	log.Println(coin)
-
 }
